48109e51199e9ae712289c45abef203f660a4248
Darklg <kev.rocher@gmail.com>
2016-05-14 17:12:41 +0200

    v 0.4.1
    * PSR1 Mode.
    * Remove dashes in field ID due to a bug in WP ( ignoring in some cases the dash in a field ID )

--- wpuusermetas.php
+++ wpuusermetas.php
@@ -4,7 +4,7 @@
 Plugin Name: WPU User Metas
 Plugin URI: http://github.com/Darklg/WPUtilities
 Description: Simple admin for user metas
-Version: 0.4
+Version: 0.4.1
 Author: Darklg
 Author URI: http://darklg.me/
 License: MIT License
@@ -12,12 +12,11 @@ License URI: http://opensource.org/licenses/MIT
 Based On: http://blog.ftwr.co.uk/archives/2009/07/19/adding-extra-user-meta-fields/
 */
 
-class WPUUserMetas
-{
+class WPUUserMetas {
     private $sections = array();
     private $fields = array();
 
-    function __construct() {
+    public function __construct() {
 
         // Admin init
         if (is_admin()) {
@@ -25,7 +24,7 @@ class WPUUserMetas
         }
     }
 
-    function admin_hooks() {
+    public function admin_hooks() {
         add_action('show_user_profile', array(&$this,
             'display_form'
         ));
@@ -42,10 +41,11 @@ class WPUUserMetas
 
     /* Datas */
 
-    function get_datas() {
+    public function get_datas() {
         $fields = apply_filters('wpu_usermetas_fields', array());
         $this->fields = array();
         foreach ($fields as $id_field => $field) {
+            $id_field = str_replace('-', '', $id_field);
             if (!isset($field['name']) || empty($field['name'])) {
                 $field['name'] = $id_field;
             }
@@ -57,7 +57,7 @@ class WPUUserMetas
         $this->sections = apply_filters('wpu_usermetas_sections', array());
     }
 
-    function get_section_fields($section_id) {
+    public function get_section_fields($section_id) {
         $fields = array();
         foreach ($this->fields as $id_field => $field) {
             if (isset($field['section']) && $field['section'] == $section_id) {
@@ -69,7 +69,7 @@ class WPUUserMetas
 
     /* Update */
 
-    function update_user_meta($user_id) {
+    public function update_user_meta($user_id) {
         if (!isset($_POST['nonce_form-usermetas']) || !wp_verify_nonce($_POST['nonce_form-usermetas'], 'form-usermetas-' . $user_id)) {
             echo __('Sorry, your nonce did not verify.');
             exit;
@@ -80,12 +80,12 @@ class WPUUserMetas
             if (isset($_POST[$id_field])) {
                 $posted_value = $_POST[$id_field];
                 switch ($field['type']) {
-                    case 'editor':
-                        $new_value = $posted_value;
-                        break;
+                case 'editor':
+                    $new_value = $posted_value;
+                    break;
 
-                    default:
-                        $new_value = esc_attr($posted_value);
+                default:
+                    $new_value = esc_attr($posted_value);
                 }
                 update_usermeta($user_id, $id_field, $new_value);
             }
@@ -94,7 +94,7 @@ class WPUUserMetas
 
     /* Display */
 
-    function display_form($user) {
+    public function display_form($user) {
         $this->get_datas();
         wp_nonce_field('form-usermetas-' . $user->ID, 'nonce_form-usermetas');
         foreach ($this->sections as $id => $section) {
@@ -102,21 +102,21 @@ class WPUUserMetas
         }
     }
 
-    function display_section($user, $id, $name) {
+    public function display_section($user, $id, $name) {
         $content = '';
         $fields = $this->get_section_fields($id);
         if (!empty($fields)) {
-            $content.= '<h3>' . $name . '</h3>';
-            $content.= '<table class="form-table">';
+            $content .= '<h3>' . $name . '</h3>';
+            $content .= '<table class="form-table">';
             foreach ($fields as $id_field => $field) {
-                $content.= $this->display_field($user, $id_field, $field);
+                $content .= $this->display_field($user, $id_field, $field);
             }
-            $content.= '</table>';
+            $content .= '</table>';
         }
         return $content;
     }
 
-    function display_field($user, $id_field, $field) {
+    public function display_field($user, $id_field, $field) {
 
         // Set vars
         $label = $field['name'];
@@ -134,42 +134,42 @@ class WPUUserMetas
         }
 
         // Add a row by field
-        $content.= '<tr>';
-        $content.= '<th><label for="' . $id_field . '">' . $label . '</label></th>';
-        $content.= '<td>';
+        $content .= '<tr>';
+        $content .= '<th><label for="' . $id_field . '">' . $label . '</label></th>';
+        $content .= '<td>';
         switch ($type) {
-            case 'editor':
-                ob_start();
-                wp_editor($value, $id_field);
-                $content.= ob_get_clean();
-                break;
+        case 'editor':
+            ob_start();
+            wp_editor($value, $id_field);
+            $content .= ob_get_clean();
+            break;
 
-            case 'textarea':
-                $content.= '<textarea rows="5" cols="30" ' . $idname . '>' . esc_attr($value) . '</textarea>';
-                break;
+        case 'textarea':
+            $content .= '<textarea rows="5" cols="30" ' . $idname . '>' . esc_attr($value) . '</textarea>';
+            break;
 
-            case 'select':
-                $content.= '<select ' . $idname . '>';
-                foreach ($datas as $val => $label) {
-                    $content.= '<option value="' . $val . '" ' . ($val == $value ? 'selected="selected"' : '') . '>' . strip_tags($label) . '</option>';
-                }
-                $content.= '</select>';
-                break;
+        case 'select':
+            $content .= '<select ' . $idname . '>';
+            foreach ($datas as $val => $label) {
+                $content .= '<option value="' . $val . '" ' . ($val == $value ? 'selected="selected"' : '') . '>' . strip_tags($label) . '</option>';
+            }
+            $content .= '</select>';
+            break;
 
-            case 'email':
-            case 'url':
-                $content.= '<input type="' . $type . '" ' . $idname . ' value="' . esc_attr($value) . '" />';
-                break;
+        case 'email':
+        case 'url':
+            $content .= '<input type="' . $type . '" ' . $idname . ' value="' . esc_attr($value) . '" />';
+            break;
 
-            default:
-                $content.= '<input type="text" ' . $idname . ' value="' . esc_attr($value) . '" />';
+        default:
+            $content .= '<input type="text" ' . $idname . ' value="' . esc_attr($value) . '" />';
         }
         if (isset($field['description'])) {
-            $content.= '<br /><span class="description">' . esc_attr($field['description']) . '</span>';
+            $content .= '<br /><span class="description">' . esc_attr($field['description']) . '</span>';
         }
 
-        $content.= '</td>';
-        $content.= '</tr>';
+        $content .= '</td>';
+        $content .= '</tr>';
         return $content;
     }
 }
